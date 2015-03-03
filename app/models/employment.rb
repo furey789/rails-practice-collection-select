edd6942f52a07204ca38d64baf2a644fc2255e2b
Peter Furey <prfurey@gmail.com>
2015-03-03 12:58:31 -0700

    Add collection_select to employment sign-up with resultig list of employment attributes to home page

--- app/models/employment.rb
+++ app/models/employment.rb
@@ -1,3 +1,8 @@
 class Employment < ActiveRecord::Base
 
+  belongs_to :person
+  belongs_to :organization
+  belongs_to :location
+  belongs_to :job_title
+
 end
--- app/models/job_title.rb
+++ app/models/job_title.rb
@@ -1,3 +1,3 @@
 class JobTitle < ActiveRecord::Base
-
+  
 end
